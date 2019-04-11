e64187ca0aed1a7a788c4d160104072b3cd9b3bd
c1erman <clerman@foxmail.com>
2019-04-11 12:19:45 +0800

    Site updated: 2019-04-11 12:19:09

--- Js-Plugin/Header-in-Hexo.js
+++ Js-Plugin/Header-in-Hexo.js
@@ -5,10 +5,20 @@ window.addEventListener('resize', toUse, false);
 function toUse() {
 	let w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 	let h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-	w <= 450 || h <="800" ? (document.getelementbyid('_menu').innerhtml header_in_block({ class: 'post-body' })) : clearspace('_menu'); } *清除占据空间* var clearspace="(id)" => {
+	block_catalog(w, h);
+	//	w <= 450 || h <="800" ? (document.getelementbyid('_menu').innerhtml header_in_block({ class: 'post-body' })) : clearspace('_menu'); w (block_catalog(w, h), }))) } *清除占据空间* var clearspace="(id)" => {
 	let box = document.getElementById(id);
 	box.innerHTML = '';
 	box.style = '';
+}
+/*为有inline-style的指定的元素display取反*/
+function anti_display() {
+	let _item = document.getElementById('_menu');
+	let to_block_regexp = /display: none|display:none/;
+	let style = _item.getAttribute('style');
+	to_block_regexp.test(style) ?
+		_item.setAttribute('style', style.replace(/display: none|display:none/, 'display:block')) :
+		_item.setAttribute('style', style.replace(/display: block|display:block/, 'display:none'));
 }
 /*选择区域内的标题列表*/
 var header_in_block = ({
@@ -27,14 +37,13 @@ var header_in_block = ({
 	box = document.getElementById(box_id);
 	/*初始样式*/
 	let styles = {
-		'H1': ['display:inline-block', 'border:none'],
-		'H2': ['display:block', 'margin-left:1%', 'border:none'],
-		'H3': ['display:block', 'margin-left:3%', 'border:none'],
-		'H4': ['display:block', 'margin-left:5%', 'border:none'],
-		'H5': ['display:block', 'margin-left:7%', 'border:none'],
-		'H6': ['display:block', 'margin-left:9%']
+		'H1': ['display:inline-block', 'margin-left:1%', 'border:none'],
+		'H2': ['display:block', 'margin-left:3%', 'border:none'],
+		'H3': ['display:block', 'margin-left:5%', 'border:none'],
+		'H4': ['display:block', 'margin-left:7%', 'border:none'],
+		'H5': ['display:block', 'margin-left:9%', 'border:none'],
+		'H6': ['display:block', 'margin-left:11%']
 	}
-	box.style = "width:82%;height:300px;overflow:auto;margin:0px auto;";
 	/*获取所有标题节点*/
 	let allHeader = new Array();
 	(Array.from(container.childNodes)).map((value, index) => {
@@ -85,6 +94,53 @@ var header_in_block = ({
 			allHeader[i].node = a;
 		}
 	}
+}
+/*将在移动端侧边添加跟随目录*/
+var block_catalog = (width, height) => {
+	const w = Math.floor(width * 0.15),
+		h = w;
+	const body = document.getElementsByTagName('body')[0];
+	/*实例按钮*/
+	let botton = document.createElement('div');
+	let botton_style = {
+		'position': 'fixed',
+		'width': `${w}px`,
+		'height': `${h}px`,
+		'left': `${-w*0.5}px`,
+		'top': `${height*0.5-w*0.5}px`,
+		'borderRadius': '50%',
+		'background': 'rgba(246,246,246,0.7)',
+		'boxShadow': '1px 0px 5px grey',
+		'cursor': 'pointer'
+	};
+	for (let i in botton_style) {
+		botton.style[i] = botton_style[i];
+	}
+	/*实例内容*/
+	let content = document.createElement('div');
+	let content_style = {
+		'display': 'none',
+		'position': 'fixed',
+		'width': `${Math.floor(width*0.8)}px`,
+		'height': `${Math.floor(height*0.9)}px`,
+		'left': `${Math.floor(width*0.1)}px`,
+		'top': `${Math.floor(height*0.03)}px`,
+		'padding': '20px 10px',
+		'overflow': 'auto',
+		'borderRadius': '10px',
+		'background': 'rgb(246,246,246)'
+	}
+	for (let i in content_style) {
+		content.style[i] = content_style[i];
+	}
+	/*给予id*/
+	content.setAttribute('id', '_menu');
+	botton.setAttribute('id', '_menu_controller');
+	/*事件监听*/
+	botton.addEventListener('click', anti_display);
+	/*插入*/
+	body.appendChild(botton);
+	body.appendChild(content);
 }
 /*数据结构：树*/
 var _NODE = function(data) {
